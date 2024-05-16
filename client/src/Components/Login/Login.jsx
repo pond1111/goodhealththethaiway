e42394b7c18fd6bca70c2c92e89d752227eb9d7a
pond1111 <127219087+pond1111@users.noreply.github.com>
2024-05-16 13:20:11 +0700

    login&server

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -38,14 +38,14 @@ const Login = () => {
                 <video src={video} autoPlay muted loop></video>
             
             <div className="textDiv">
-                <h2 className="title">Create And Sell Extraordinary Products</h2>
-                <p>Adopt the peace of nature!</p>
+                <h2 className="title">ร่างกายดี ชีวิตดี มีความสุข</h2>
+                <p>มาจดบันทึกการเปลี่ยนแปลงของร่างกายกัน</p>
             </div>
 
             <div className="footerDiv flex">
-             <span className="text"> Don't have a account? </span>
+             <span className="text"> ยังไม่มี บัญชี? </span>
              <Link to={'/register'}>
-             <button className="btn">Sign Up</button>
+             <button className="btn">สมัคร</button>
              </Link>
             </div>
             </div>
@@ -53,12 +53,12 @@ const Login = () => {
             <div className="formDiv flex">
                 <div className="headerDiv">
                     < img src={logo} alt="logo image" />
-                    <h3>Welcome Back!</h3>
+                    <h3> </h3>
                 </div>
 
                 <form action="" className='form grid'>
                     <div className='inputDiv'>
-                     <label htmlFor="username">Usernsme</label>
+                     <label htmlFor="username">ขื่อ ผู้ใช้</label>
                      <div className="input flex">
                      <FaUserShield className='icon'/>
                      <input type="text" id='username' placeholder="Enter Username"
@@ -69,7 +69,7 @@ const Login = () => {
                     </div>
 
                     <div className='inputDiv'>
-                     <label htmlFor="username">Phone</label>
+                     <label htmlFor="username">เบอร์โทร</label>
                      <div className="input flex">
                      <BsFillShieldLockFill  className='icon'/>
                      <input type="phone" id='phone' placeholder="Enter Phone"
